cc3ec92e405b197e26558d24415d773abd812297
Lyze <11274700+lyze237@users.noreply.github.com>
2024-06-16 23:05:35 +0200

    Implemented water tiling background

--- core/src/main/java/dev/lyze/festive/game/background/BackgroundBehaviour.java
+++ core/src/main/java/dev/lyze/festive/game/background/BackgroundBehaviour.java
@@ -1,23 +1,67 @@
 package dev.lyze.festive.game.background;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import dev.lyze.festive.game.Assets;
 import dev.lyze.gdxUnBox2d.GameObject;
 import dev.lyze.gdxUnBox2d.behaviours.BehaviourAdapter;
+import lombok.AllArgsConstructor;
+
+import java.util.LinkedList;
 
 public class BackgroundBehaviour extends BehaviourAdapter {
-
     private final ExtendViewport viewport;
+
+    private final LinkedList<Background> backgrounds = new LinkedList<>();
 
     public BackgroundBehaviour(ExtendViewport viewport, GameObject gameObject) {
         super(gameObject);
 
         this.viewport = viewport;
 
-        new WaterBackgroundBehaviour(0, 0, Assets.getStartIsland(), Assets.getRandomWaterTile(), this.viewport, new GameObject("Start Island", getUnBox()));
+        // backgrounds.add(new Background(0, 0, Assets.getStartIsland(), Assets.getRandomWaterTile()));
+        for (int i = 0; i < 3; i++) {
+            backgrounds.add(new Background(i, 0, null, Assets.getRandomWaterTile()));
+        }
     }
 
     @Override
     public void update(float delta) {
+        var cameraPosition = viewport.getCamera().position;
+
+        var mostLeftX = backgrounds.getFirst().x;
+        var mostRightX = backgrounds.getLast().x;
+
+        if (cameraPosition.x > mostRightX * viewport.getMinWorldWidth()) {
+            backgrounds.removeFirst();
+            backgrounds.addLast(new Background(mostRightX + 1, 0, null, Assets.getRandomWaterTile()));
+        }
+
+        if (cameraPosition.x - viewport.getWorldWidth() / 2f < mostLeftX * viewport.getMinWorldWidth()) {
+            backgrounds.removeLast();
+            backgrounds.addFirst(new Background(mostLeftX - 1, 0, null, Assets.getRandomWaterTile()));
+        }
+    }
+
+    @Override
+    public void render(Batch batch) {
+        for (int i = backgrounds.size() - 1; i >= 0; i--) {
+            backgrounds.get(i).draw(viewport, batch);
+        }
+    }
+
+    @AllArgsConstructor
+    private static class Background {
+        private int x, y;
+        private Texture overlay;
+        private Texture texture;
+
+        public void draw(ExtendViewport viewport, Batch batch) {
+            batch.draw(texture, x * viewport.getMinWorldWidth(), y * viewport.getMinWorldHeight(), viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
+
+            if (overlay != null)
+                batch.draw(overlay, x * viewport.getMinWorldWidth(), y * viewport.getMinWorldHeight(), viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
+        }
     }
 }
--- core/src/main/java/dev/lyze/festive/game/background/WaterBackgroundBehaviour.java
+++ core/src/main/java/dev/lyze/festive/game/background/WaterBackgroundBehaviour.java
@@ -1,61 +1,40 @@
 package dev.lyze.festive.game.background;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import dev.lyze.gdxUnBox2d.GameObject;
 import dev.lyze.gdxUnBox2d.behaviours.BehaviourAdapter;
-import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class WaterBackgroundBehaviour extends BehaviourAdapter {
-    private final float x, y;
-    private final Texture overlay;
-    private final Texture water;
+    private final int offset;
     private final ExtendViewport viewport;
+    private final Texture overlay;
 
-    public WaterBackgroundBehaviour(float x, float y, Texture water, ExtendViewport viewport, GameObject gameObject) {
-        this(x, y, null, water, viewport, gameObject);
+    private float renderX, renderY;
+
+    public WaterBackgroundBehaviour(int offset, ExtendViewport viewport, GameObject gameObject) {
+        this(offset, viewport, null, gameObject);
     }
 
-    public WaterBackgroundBehaviour(float x, float y, Texture overlay, Texture water, ExtendViewport viewport, GameObject gameObject) {
+    public WaterBackgroundBehaviour(int offset, ExtendViewport viewport, Texture overlay, GameObject gameObject) {
         super(gameObject);
 
-        this.x = x;
-        this.y = y;
-        this.overlay = overlay;
-        this.water = water;
+        this.offset = offset;
         this.viewport = viewport;
+        this.overlay = overlay;
     }
 
     @Override
     public void update(float delta) {
-        if (viewport.getCamera().position.x - viewport.getWorldWidth() / 2f > x + viewport.getWorldWidth())
-            getGameObject().destroy();
+
     }
 
     @Override
     public void render(Batch batch) {
-        batch.draw(water, x, y, viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
+        // batch.draw(texture, renderX, renderY, viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
 
         if (overlay != null)
-            batch.draw(overlay, x, y, viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
-     }
-
-    @Override
-    public void debugRender(ShapeRenderer renderer) {
-        renderer.setColor(Color.GREEN);
-        renderer.circle(viewport.getCamera().position.x, viewport.getCamera().position.y, 0.2f, 10);
-        renderer.setColor(Color.RED);
-        renderer.circle(x, y, 0.2f, 10);
-        renderer.circle(x + viewport.getWorldWidth(), y, 0.2f, 10);
-
-        renderer.setColor(Color.ORANGE);
-        renderer.rect(x, y, 16, 9);
-
-        renderer.setColor(Color.PURPLE);
-        renderer.rect(viewport.getCamera().position.x - viewport.getWorldWidth() / 2f, viewport.getCamera().position.y - viewport.getWorldHeight() / 2f, viewport.getWorldWidth(), viewport.getWorldHeight());
+            batch.draw(overlay, renderX, renderY, viewport.getMinWorldWidth(), viewport.getMinWorldHeight());
     }
 }
