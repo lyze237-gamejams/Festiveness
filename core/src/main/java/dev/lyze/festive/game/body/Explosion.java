a69c7caf25599f6a1b2af8f15eedc955621cb322
Lyze <11274700+lyze237@users.noreply.github.com>
2024-06-16 17:43:46 +0200

    Implemented test explosion

--- core/src/main/java/dev/lyze/festive/game/body/Explosion.java
+++ core/src/main/java/dev/lyze/festive/game/body/Explosion.java
@@ -2,10 +2,16 @@ package dev.lyze.festive.game.body;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import dev.lyze.festive.Constants;
 import dev.lyze.festive.ViewportBehaviour;
+import dev.lyze.gdxUnBox2d.Box2dBehaviour;
 import dev.lyze.gdxUnBox2d.GameObject;
 import dev.lyze.gdxUnBox2d.behaviours.BehaviourAdapter;
+import dev.lyze.gdxUnBox2d.behaviours.fixtures.CreateCircleFixtureBehaviour;
 
 public class Explosion extends BehaviourAdapter {
     private final Vector2 input = new Vector2();
@@ -38,10 +44,24 @@ public class Explosion extends BehaviourAdapter {
         if (!clicked)
             return;
 
-        System.out.println("HI");
         clicked = false;
-        viewport.getViewport().project(input);
+        viewport.getViewport().unproject(input);
 
-        player.getStomach2().getBody().applyLinearImpulse(0.000001f, 0.00001f, input.x, input.y, true);
+        createExplosion();
+    }
+
+    private void createExplosion() {
+        for (int i = 0; i < 25; i++) {
+            var ball = new GameObject(getUnBox());
+            var bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+            bodyDef.position.set(input);
+            bodyDef.linearVelocity.set(new Vector2(10,0).rotateDeg(MathUtils.random(360)));
+            new Box2dBehaviour(bodyDef, ball);
+            var fixtureDef = new FixtureDef();
+            fixtureDef.filter.categoryBits = Constants.Bit_Ground;
+            fixtureDef.filter.maskBits = (short) (Constants.Bit_Enemies | Constants.Bit_PlayerFront | Constants.Bit_PlayerBack | Constants.Bit_Tools);
+            new CreateCircleFixtureBehaviour(new Vector2(), .1f, fixtureDef, ball);
+        }
     }
 }
